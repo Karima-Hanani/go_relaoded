4c9a9a0a58e13ece8d37a40c1fe32cf62ff59cce
khanani <karimahanani398@gmail.com>
2025-11-23 22:09:36 +0100

    commit

---
+++ functions/flag.go
@@ -0,0 +1,55 @@
+package project
+
+import (
+	"strconv"
+	"strings"
+)
+
+func Flag(line []string) []string {
+	for i := 0; i < len(line); i++ {
+		op := line[i]
+		if op == "(cap)" || op == "(low)" || op == "(up)" {
+			if i != 0 {
+				switch op {
+				case "(cap)":
+					line[i-1] = Capitalize(line[i-1])
+				case "(low)":
+					line[i-1] = strings.ToLower(line[i-1])
+				case "(up)":
+					line[i-1] = strings.ToUpper(line[i-1])
+				}
+			}
+			line = append(line[:i], line[i+1:]...)
+			i--
+			continue
+		}
+		if op == "(hex)" || op == "(bin)" {
+			if i != 0 {
+				switch op {
+				case "(hex)":
+					n, err := strconv.ParseInt(line[i-1], 16, 64)
+					if err != nil {
+						line = append(line[:i], line[i+1:]...)
+						i--
+						continue
+					} else {
+						line[i-1] = strconv.Itoa(int(n))
+					}
+				case "(bin)":
+					n, err := strconv.ParseInt(line[i-1], 2, 64)
+					if err != nil {
+						line = append(line[:i], line[i+1:]...)
+						i--
+						continue
+					} else {
+						line[i-1] = strconv.Itoa(int(n))
+					}
+				}
+			}
+			line = append(line[:i], line[i+1:]...)
+			i--
+			continue
+		}
+	}
+	return line
+}
